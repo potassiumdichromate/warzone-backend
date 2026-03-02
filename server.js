e1b557fd567ca38f55d5095374912a16703c452a
BigClitPhobia <sidhanth6am@gmail.com>
2026-03-02 11:22:59 +0530

    changes cors

--- server.js
+++ server.js
@@ -31,10 +31,11 @@ const allowedOrigins = [
   'http://warzonewarriors.xyz',
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://warzonewarrior.vercel.app/',
+  'http://localhost:5173',
+  'http://localhost:5174',
   'https://warzonewarrior.vercel.app',
   'https://pub-2c48e58780b648b7a2a77316f7b0aa2c.r2.dev',
-  'https://warzone-admin.vercel.app/'
+  'https://warzone-admin.vercel.app',
 ];
 
 const corsOptions = {
