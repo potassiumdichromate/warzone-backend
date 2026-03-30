2ac891b881a6c2887a72f849002998adbd72acdf
Rishabh Jaiswal <rishabhjaiswal@Rishabhs-MacBook-Air.local>
2026-03-30 12:41:36 +0530

    aded chagnes

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'https://warzonewarrior.vercel.app',
   'https://warzonewarrior-chinese.vercel.app',
+  'https://magical-truffle-9ecd00.netlify.app/',
   'https://cn.warzonewarriors.xyz',
   'https://warzone-admin.vercel.app/',
   'https://pub-2c48e58780b648b7a2a77316f7b0aa2c.r2.dev'
